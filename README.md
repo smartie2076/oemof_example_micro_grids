f89da099cd8b3894f91fc0f42a74c47e61c7bc2a
smartie2076 <martha.hoffmann@rl-institut.de>
2019-04-15 11:48:39 +0200

    Added readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# oemof_example_micro_grids
+Oemof coding example: Optimizing micro grids
+
+## General description
+Based on example from the SDEWES conference paper:
+
+Simon Hilpert, Cord Kaldemeyer, Uwe Krien, Stephan Günther (2017).
+'Solph - An Open Multi Purpose Optimisation Library for Flexible
+         Energy System Analysis'. Paper presented at SDEWES Conference,
+         Dubrovnik.
+
+## Data
+timeseries.csv
+
+## Installation requirements
+This example requires the latest version of oemof and others. Install by:
+
+    pip install oemof matplotlib
--- micro_grid_optimization.py
+++ micro_grid_optimization.py
@@ -1,26 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-General description
--------------------
-Based on example from the SDEWES conference paper:
-
-Simon Hilpert, Cord Kaldemeyer, Uwe Krien, Stephan Günther (2017).
-'Solph - An Open Multi Purpose Optimisation Library for Flexible
-         Energy System Analysis'. Paper presented at SDEWES Conference,
-         Dubrovnik.
-
-Data
-----
-timeseries.csv
-
-Installation requirements
--------------------------
-This example requires the latest version of oemof and others. Install by:
-
-    pip install oemof matplotlib
-
-"""
-
 import os
 import pandas as pd
 
